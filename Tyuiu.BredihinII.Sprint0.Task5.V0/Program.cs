c5bcb874f244fe556b18b323ceff6b3b6971040e
nomad <nomad@DESKTOP-8M9J1CP>
2025-09-16 21:53:54 +0500

    Task5

---
+++ Tyuiu.BredihinII.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BredihinII.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.BredihinII.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(5, 16));
+            Console.WriteLine("A - B = " + DataService.Subtraction(22, 3));
+            Console.WriteLine("A * B = " + DataService.Multiplication(6, 7));
+            Console.WriteLine("A / B = " + DataService.Division(144, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
