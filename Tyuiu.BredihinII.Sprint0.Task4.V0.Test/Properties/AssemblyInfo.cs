fee45d05890752a096a30481d83a6ac4e349d931
nomad <nomad@DESKTOP-8M9J1CP>
2025-09-16 21:42:20 +0500

    Added Task 4

---
+++ Tyuiu.BredihinII.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BredihinII.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BredihinII.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b84e2152-0713-4a23-a6bf-dc9196601236")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.BredihinII.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BredihinII.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.BredihinII.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(5, 16));
+            Console.WriteLine(DataService.Subtraction(22, 3));
+            Console.WriteLine(DataService.Multiplication(6, 7));
+            Console.WriteLine(DataService.Division(144, 2));
+
+            Console.ReadKey();
+        }
+    }
+}
