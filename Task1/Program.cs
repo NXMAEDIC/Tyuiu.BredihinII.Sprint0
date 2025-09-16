6833750e129144e01758907e4d1530cab35ce9e8
nomad <nomad@DESKTOP-8M9J1CP>
2025-09-16 20:39:56 +0500

    Added Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Илья";
+            int age = 23;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " года!");
+            Console.ReadKey();
+        }
+    }
+}
